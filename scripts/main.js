237ba1b8b1683ec13558b2d5a4e6f0cfe495de34
reznoa <reznoa@gmail.com>
2016-05-25 14:27:39 +0900

    fix header title visual

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 $(function(){
 	$("#site-home .site-icon").hover(
-		function() { $("#site-home .site-title").css("visibility", "visible"); },
-		function() { $("#site-home .site-title").css("visibility", "hidden"); }
+		function() {
+			$("#site-home .site-title").css("visibility", "visible");
+			$("#site-nav ul li a").css("opacity", "0.5");
+		},
+		function() {
+			$("#site-home .site-title").css("visibility", "hidden");
+			$("#site-nav ul li a").css("opacity", "1");
+		}
 	);
 });
